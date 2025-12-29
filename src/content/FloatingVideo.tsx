3124b6f07262408e72294c6ec5ffdaf4eae60c5f
bryndioleksandr <s.v.bryndo@gmail.com>
2025-12-29 18:38:59 +0200

    feat: init object detector, train to work with mediapipe

--- src/content/FloatingVideo.tsx
+++ src/content/FloatingVideo.tsx
@@ -1,4 +1,14 @@
-import React, {useRef, useState, useEffect} from "react";
+import {useRef, useState, useEffect} from "react";
+import {ObjectDetector, FilesetResolver, ObjectDetectorResult} from "@mediapipe/tasks-vision";
+
+type RunningMode = "IMAGE" | "VIDEO"
+
+let objectDetector: ObjectDetector;
+let runningMode: RunningMode = "IMAGE";
+let video = document.getElementById("video-player") as HTMLVideoElement;
+let videoParent = document.getElementById("video-container") as HTMLDivElement;
+let lastVideoTime = -1;
+
 
 export const FloatingVideo = () => {
     const videoRef = useRef<HTMLVideoElement>(null);
@@ -6,6 +16,29 @@ export const FloatingVideo = () => {
 
     const [error, setError] = useState<string | null>(null);
     const [isCameraOn, setIsCameraOn] = useState<boolean>(false);
+    const [isModelLoaded, setIsModelLoaded] = useState<boolean>(false);
+
+    const initObjDetector = async () => {
+        try {
+            console.log('trying to init');
+            const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2/wasm");
+            objectDetector = await ObjectDetector.createFromOptions(vision, {
+                baseOptions: {
+                    modelAssetPath: `https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/float16/1/efficientdet_lite0.tflite`,
+                    delegate: "GPU"
+                },
+                scoreThreshold: 0.5,
+                runningMode: "VIDEO"
+            });
+            setIsModelLoaded(true);
+        } catch (e) {
+            console.error("error is:", e);
+        }
+    }
+
+    useEffect(() => {
+        initObjDetector();
+    }, []);
 
     useEffect(() => {
         const startCamera = async () => {
@@ -37,10 +70,13 @@ export const FloatingVideo = () => {
             }
         };
 
-        if (isCameraOn) {
-            startCamera();
-        } else {
-            stopCamera();
+        if (isModelLoaded) {
+            console.log('object detector is here:', objectDetector);
+            if (isCameraOn) {
+                startCamera();
+            } else {
+                stopCamera();
+            }
         }
 
         return () => {
@@ -48,8 +84,29 @@ export const FloatingVideo = () => {
         };
     }, [isCameraOn]);
 
+    const detectObject = async () => {
+       let startTimeMs = performance.now();
+       if(videoRef.current?.currentTime !== lastVideoTime && videoRef.current?.currentTime) {
+           lastVideoTime = videoRef.current?.currentTime;
+           const detections = objectDetector.detectForVideo(video, startTimeMs);
+           // call function and send detections to it
+       }
+       window.requestAnimationFrame(detectObject);
+    }
+
+    var children:any[] = [];
+
+    const displayHighlightedDetections = (detections: ObjectDetectorResult) => {
+        for (let child of children) {
+            videoParent.removeChild(child);
+        }
+        children.splice(0);
+
+
+    }
+
     return (
-        <div className="video-container flex flex-col justify-center text-center">
+        <div id="video-container" className="video-container flex flex-col justify-center text-center">
             <h2>Камера</h2>
             <button onClick={() => {
                 setIsCameraOn(!isCameraOn)
@@ -62,12 +119,13 @@ export const FloatingVideo = () => {
 
                 isCameraOn ? (
                     <video
-                    ref={videoRef}
-                    autoPlay
-                    playsInline
-                    muted
-                    className="video-player w-full max-w-[600px] rounded-[10px] border m-auto"
-                />) : null
+                        ref={videoRef}
+                        autoPlay
+                        playsInline
+                        muted
+                        id="video-player"
+                        className="video-player w-full max-w-[600px] rounded-[10px] border m-auto"
+                    />) : null
 
             )}
         </div>
