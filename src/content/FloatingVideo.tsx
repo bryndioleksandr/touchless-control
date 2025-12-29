bb69b392f72c9488a9580a36d0ebd66bff36f85f
bryndioleksandr <s.v.bryndo@gmail.com>
2025-12-29 23:49:50 +0200

    refactor: remembered about refs in react; feat: add simple rect to detect and track objects; todo: make it faster

--- src/content/FloatingVideo.tsx
+++ src/content/FloatingVideo.tsx
@@ -1,18 +1,22 @@
 import {useRef, useState, useEffect} from "react";
-import {ObjectDetector, FilesetResolver, ObjectDetectorResult} from "@mediapipe/tasks-vision";
+import {ObjectDetector, FilesetResolver, type ObjectDetectorResult} from "@mediapipe/tasks-vision";
 
-type RunningMode = "IMAGE" | "VIDEO"
 
 let objectDetector: ObjectDetector;
-let runningMode: RunningMode = "IMAGE";
-let video = document.getElementById("video-player") as HTMLVideoElement;
-let videoParent = document.getElementById("video-container") as HTMLDivElement;
+// let video = document.getElementById("video-player") as HTMLVideoElement;
+// let videoParent = document.getElementById("video-container") as HTMLDivElement;
+// let canvas = document.getElementById("over-video") as HTMLCanvasElement;
+// const canvaCtx = canvas.getContext("2d");
 let lastVideoTime = -1;
 
 
 export const FloatingVideo = () => {
     const videoRef = useRef<HTMLVideoElement>(null);
     const streamRef = useRef<MediaStream | null>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
+    const videoContainerRef = useRef<HTMLDivElement>(null);
+
 
     const [error, setError] = useState<string | null>(null);
     const [isCameraOn, setIsCameraOn] = useState<boolean>(false);
@@ -35,6 +39,15 @@ export const FloatingVideo = () => {
             console.error("error is:", e);
         }
     }
+
+    useEffect(() => {
+        if (!canvasRef.current) return;
+
+        const ctx = canvasRef.current.getContext("2d");
+        if (!ctx) return;
+
+        ctxRef.current = ctx;
+    }, [canvasRef, isCameraOn]);
 
     useEffect(() => {
         initObjDetector();
@@ -70,8 +83,8 @@ export const FloatingVideo = () => {
             }
         };
 
+
         if (isModelLoaded) {
-            console.log('object detector is here:', objectDetector);
             if (isCameraOn) {
                 startCamera();
             } else {
@@ -86,10 +99,12 @@ export const FloatingVideo = () => {
 
     const detectObject = async () => {
        let startTimeMs = performance.now();
-       if(videoRef.current?.currentTime !== lastVideoTime && videoRef.current?.currentTime) {
+       if(videoRef.current?.currentTime !== lastVideoTime && videoRef.current?.currentTime && objectDetector) {
            lastVideoTime = videoRef.current?.currentTime;
-           const detections = objectDetector.detectForVideo(video, startTimeMs);
+           const detections = objectDetector.detectForVideo(videoRef.current, startTimeMs);
+           console.log('detections:', detections);
            // call function and send detections to it
+           displayHighlightedDetections(detections);
        }
        window.requestAnimationFrame(detectObject);
     }
@@ -98,15 +113,39 @@ export const FloatingVideo = () => {
 
     const displayHighlightedDetections = (detections: ObjectDetectorResult) => {
         for (let child of children) {
-            videoParent.removeChild(child);
+            videoContainerRef.current?.removeChild(child);
         }
         children.splice(0);
+        if(canvasRef.current && ctxRef.current) {
+            // console.log('drawing DRAWING', detections.detections[0].boundingBox?.originX as number, detections.detections[0].boundingBox?.originY as number, detections.detections[0].boundingBox?.width as number, detections.detections[0].boundingBox?.height as number)
+            const ctx = ctxRef.current;
+            ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+            ctx.fillStyle = "rgba(88, 231, 0, 0.5)";
+            ctx.fillRect(detections.detections[0].boundingBox?.originX as number, detections.detections[0].boundingBox?.originY as number, detections.detections[0].boundingBox?.width as number, detections.detections[0].boundingBox?.height as number)
+            // if(!(detections.detections[0].boundingBox)) return;
+            // const { originX, originY, width, height } = detections.detections[0].boundingBox;
 
+            // ctx.beginPath();
+            // ctx.lineWidth = 4;
+            // ctx.strokeStyle = "#00FF00";
+            // ctx.rect(originX, originY, width-10, height);
+            // ctx.stroke();
+            //
+            // ctx.fillStyle = "rgba(0, 255, 0, 0.1)";
+            // ctx.fill();
+
+            // if (detections.detections[0].categories[0]) {
+            //     const label = `${detections.detections[0].categories[0].categoryName} ${Math.round(detections.detections[0].categories[0].score * 100)}%`;
+            //     ctx.fillStyle = "#00FF00";
+            //     ctx.font = "20px Arial";
+            //     ctx.fillText(label, originX, originY - 10);
+            // }
+        }
 
     }
 
     return (
-        <div id="video-container" className="video-container flex flex-col justify-center text-center">
+        <div id="video-container" ref={videoContainerRef} className="video-container relative flex flex-col justify-center text-center">
             <h2>Камера</h2>
             <button onClick={() => {
                 setIsCameraOn(!isCameraOn)
@@ -118,14 +157,25 @@ export const FloatingVideo = () => {
             ) : (
 
                 isCameraOn ? (
+                    <div className="video-container relative w-full max-w-[600px] rounded-[10px] overflow-hidden border border-black mx-auto">
                     <video
                         ref={videoRef}
                         autoPlay
                         playsInline
                         muted
                         id="video-player"
-                        className="video-player w-full max-w-[600px] rounded-[10px] border m-auto"
-                    />) : null
+                        className="w-full h-auto block"
+                        onLoadedData={() => {
+                            if (canvasRef.current && videoRef.current) {
+                                canvasRef.current.width = videoRef.current.videoWidth;
+                                canvasRef.current.height = videoRef.current.videoHeight;
+                            }
+                            console.log('start detection');
+                            detectObject();
+                        }}
+                    />
+                        <canvas id="over-video" ref={canvasRef} className="absolute top-0 left-0 pointer-events-none flex items-center justify-center text-white font-bold bg-black/20"/>
+                    </div>) : null
 
             )}
         </div>
